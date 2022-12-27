1dc99167954abf03aa2e6a1308c651428cd1ebde
Oleg <olberd@bk.ru>
2022-12-27 21:56:04 +0300

    Load dvmn.svg

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def load_image():
+    url = 'https://dvmn.org/filer/canonical/1542890876/16/'
+    response = requests.get(url)
+    response.raise_for_status()
+    filename = 'dvmn.svg'
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+if __name__ == '__main__':
+    load_image()
