016ba791f90189fa800e428432cb4f3edbb385ae
Oleg <olberd@bk.ru>
2023-01-12 22:12:55 +0300

    Download books from tululu.org.

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import requests
+import urllib3
+from pathlib import Path
 
 
-def load_image():
-    url = 'https://dvmn.org/filer/canonical/1542890876/16/'
-    response = requests.get(url)
+urllib3.disable_warnings()
+
+book_url = 'https://tululu.org/txt.php'
+
+
+def load_book(url, book_file):
+    payload = {
+        'id': book_file
+    }
+    response = requests.get(url, params=payload, verify=False)
     response.raise_for_status()
-    filename = 'dvmn.svg'
-    with open(filename, 'wb') as file:
+    filename = f'id{book_file}.txt'
+    path = Path('books', filename)
+    path.parent.mkdir(exist_ok=True, parents=True)
+    with path.open('wb') as file:
         file.write(response.content)
 
 
+def get_books():
+    for book in range(1, 11):
+        load_book(book_url, book)
+
+
 if __name__ == '__main__':
-    load_image()
+    # load_book(book_url)
+    get_books()
