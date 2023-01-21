51cba10463b53951e7a7666528f38ac21571fab1
Oleg <olberd@bk.ru>
2023-01-21 15:49:58 +0300

    Download book covers.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os.path
+from urllib.parse import urljoin, unquote, urlsplit
 import requests
 import logging
 import urllib3
@@ -10,7 +11,7 @@ from pathvalidate import sanitize_filename
 urllib3.disable_warnings()
 
 BOOK_TXT_URL = 'https://tululu.org/txt.php'
-BOOK_NAME_URL = 'https://tululu.org/b'
+BOOK_PAGE_URL = 'https://tululu.org/b'
 
 
 def check_for_redirect(response):
@@ -18,10 +19,13 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def parse_title_book(url, book_id):
-    book_description_url = f'{url}{book_id}'
-    response = requests.get(book_description_url)
+def get_book_page_soup(url):
+    response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
+    return soup
+
+
+def parse_title_book(soup):
     title = soup.find('div', id='content').find('h1').text
     title = title.split('::')
     book_name = title[0].strip()
@@ -39,6 +43,23 @@ def download_txt(url, filename, folder='books/'):
     return os.path.join(folder, filename)
 
 
+def get_image_url(book_description_url, soup):
+    image_url = soup.find('div', class_='bookimage').find('img')['src']
+    full_image_url = urljoin(book_description_url, image_url)
+    return full_image_url
+
+
+def download_image(url, folder='images/'):
+    response = requests.get(url)
+    response.raise_for_status()
+    filename = unquote(urlsplit(url).path)
+    filename = filename.split('/')[-1]
+    path = Path(folder, filename)
+    path.parent.mkdir(exist_ok=True, parents=True)
+    with path.open('wb') as file:
+        file.write(response.content)
+
+
 def main():
     logging.basicConfig(level=logging.ERROR)
     for book_id in range(1, 11):
@@ -50,7 +71,13 @@ def main():
         except requests.HTTPError:
             logging.error(f'Нет книги с id {book_id}')
             continue
-        download_txt(response.url, f'{book_id}. {parse_title_book(BOOK_NAME_URL, book_id)}')
+        book_description_url = f'{BOOK_PAGE_URL}{book_id}'
+        book_description_response = requests.get(book_description_url)
+        response.raise_for_status()
+        soup = BeautifulSoup(book_description_response.text, 'lxml')
+
+        download_txt(response.url, f'{book_id}. {parse_title_book(soup)}')
+        download_image(get_image_url(book_description_url, soup))
 
 
 if __name__ == '__main__':
