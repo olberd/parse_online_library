92f3d88c897daeea120cb4c1f22f8f99c8d0d405
Oleg <olberd@bk.ru>
2023-02-02 23:25:37 +0300

    Corrections.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-import os.path
+import time
 from urllib.parse import urljoin, unquote, urlsplit
 import requests
 import logging
@@ -8,7 +8,6 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 
-
 urllib3.disable_warnings()
 
 BOOK_TXT_URL = 'https://tululu.org/txt.php'
@@ -16,33 +15,17 @@ BOOK_PAGE_URL = 'https://tululu.org/b'
 
 
 def check_for_redirect(response):
-    if response.history and response.url == 'https://tululu.org/':
+    if response.url == 'https://tululu.org/':
         raise requests.HTTPError
 
 
-def get_book_page_soup(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    return soup
-
-
-def parse_title_author_book(soup):
-    title = soup.find('div', id='content').find('h1').text
-    title = title.split('::')
-    book_name = title[0].strip()
-    book_author = title[1].strip()
-    return book_name, book_author
-
-
-def download_txt(url, filename, folder='books/'):
-    response = requests.get(url)
-    response.raise_for_status()
+def download_txt(response, filename, folder='books/'):
     filename = f'{sanitize_filename(filename)}.txt'
     path = Path(folder, filename)
     path.parent.mkdir(exist_ok=True, parents=True)
     with path.open('wb') as file:
         file.write(response.content)
-    return os.path.join(folder, filename)
+    return path
 
 
 def get_image_url(book_description_url, soup):
@@ -62,58 +45,51 @@ def download_image(url, folder='images/'):
         file.write(response.content)
 
 
-def download_comments(soup):
-    texts = soup.find_all(class_='texts')
-    comments = []
-    for com in texts:
-        comment = com.find('span').text
-        comments.append(comment)
-    return comments
-
-
-def get_genres(soup):
-    genres_links = soup.find('span', class_='d_book').find_all('a')
-    genres = []
-    for genre in genres_links:
-        genres.append(genre.text)
-    return genres
-
-
 def parse_book_page(soup):
+    title = soup.find('div', id='content').find('h1').text
+    title, author = title.split('::')
+    genres_links = soup.find('span', class_='d_book').find_all('a')
+    genres = [genre.text for genre in genres_links]
+    texts = soup.find_all(class_='texts')
+    comments = [com.find('span').text for com in texts]
     book_description = {
-        'name': parse_title_author_book(soup)[0],
-        'author': parse_title_author_book(soup)[1],
-        'genres': get_genres(soup),
+        'title': title.strip(),
+        'author': author.strip(),
+        'comments': comments,
+        'genres': genres,
     }
     return book_description
 
 
-def main(start_id, end_id):
+def main():
     logging.basicConfig(level=logging.ERROR)
-    for book_id in range(start_id, end_id+1):
-        payload = {'id': book_id}
-        response = requests.get(BOOK_TXT_URL, params=payload, verify=False)
-        response.raise_for_status()
+    parser = argparse.ArgumentParser(description='Скачивает файлы с текстами и обложками книг с сайта tululu.org')
+    parser.add_argument('start_id', default=1, type=int, help='с какого номера id книги начать загрузку')
+    parser.add_argument('end_id', default=10, type=int, help='по какой id книги закончить загрузку')
+    args = parser.parse_args()
+    for book_id in range(args.start_id, args.end_id + 1):
         try:
+            payload = {'id': book_id}
+            response = requests.get(BOOK_TXT_URL, params=payload, verify=False)
+            response.raise_for_status()
             check_for_redirect(response)
+            book_description_url = f'{BOOK_PAGE_URL}{book_id}'
+            book_description_response = requests.get(book_description_url)
+            book_description_response.raise_for_status()
+            check_for_redirect(book_description_response)
+            soup = BeautifulSoup(book_description_response.text, 'lxml')
+            book_description = parse_book_page(soup)
+            download_txt(response, f'{book_id}. {book_description["title"]}')
+            download_image(get_image_url(book_description_url, soup))
+            print(book_description)
         except requests.HTTPError:
             logging.error(f'Нет книги с id {book_id}')
             continue
-        book_description_url = f'{BOOK_PAGE_URL}{book_id}'
-        book_description_response = requests.get(book_description_url)
-        response.raise_for_status()
-        soup = BeautifulSoup(book_description_response.text, 'lxml')
-
-        download_txt(response.url, f'{book_id}. {parse_title_author_book(soup)}')
-        download_image(get_image_url(book_description_url, soup))
-        print(parse_title_author_book(soup))
-        print(download_comments(soup))
-        print(get_genres(soup))
+        except requests.ConnectionError:
+            logging.error(f'Нет подключения к сайту tululu.org')
+            time.sleep(15)
+            continue
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Скачивает файлы с книгами с сайта tululu.org')
-    parser.add_argument('start_id', default=1, type=int)
-    parser.add_argument('end_id', default=10, type=int)
-    args = parser.parse_args()
-    main(args.start_id, args.end_id)
+    main()
