7ed8012febf1203102bc8ed579f9cab1a7c9286c
Oleg <olberd@bk.ru>
2023-03-24 20:54:47 +0300

    Fix genres buttons.

--- render_website.py
+++ render_website.py
@@ -9,7 +9,7 @@ from more_itertools import chunked
 NUM_BOOKS_ON_PAGE = 10
 
 with open('books_descriptions.json', 'r', encoding='utf8') as file:
-    books = json.load(file)
+    books_descr = json.load(file)
 
 
 def split_books_by_pages():
@@ -19,11 +19,11 @@ def split_books_by_pages():
         autoescape=select_autoescape(['html'])
     )
     template = env.get_template('template.html')
-    books_chunk = chunked(books, NUM_BOOKS_ON_PAGE)
-    pages_amount = math.ceil(len(books) / NUM_BOOKS_ON_PAGE) + 1
-    for idx_page, books_page in enumerate(books_chunk, start=1):
+    books_descr_chunk = chunked(books_descr, NUM_BOOKS_ON_PAGE)
+    pages_amount = math.ceil(len(books_descr) / NUM_BOOKS_ON_PAGE) + 1
+    for idx_page, books_page in enumerate(books_descr_chunk, start=1):
         rendered_page = template.render(
-            books=books_page,
+            books_descr=books_page,
             pages_amount=pages_amount,
             current_page=idx_page,
         )
